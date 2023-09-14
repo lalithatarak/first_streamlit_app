eb69d05ae4dbb708c4f09b4205f03e48ce6753b2
lalithatarak <145041515+lalithatarak@users.noreply.github.com>
2023-09-14 13:18:44 -0400

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -1,7 +1,8 @@
 import streamlit
 
 streamlit.title('my parents new healthy dinner')
-streamlit.header('Breakfast menu')
-streamlit.text('omega-3 blueberry oatmeal')
-streamlit.text('kale,spinach&rocket smoothie')
-streamlit.text('hard boiled free-ramged egg')
+streamlit.header('Breakfast Favotite')
+streamlit.text('🥣 omega-3 blueberry oatmeal')
+streamlit.text('🥗 kale,spinach&rocket smoothie')
+streamlit.text('🐔 hard boiled free-ramged egg')
+streamlit.text('🥑🍞 Avacado toast')
