c8c4630f196a848493de38da6d43baf0c3a5cb99
lalithatarak <145041515+lalithatarak@users.noreply.github.com>
2023-09-14 13:55:32 -0400

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -1,12 +1,4 @@
-import streamlit
 
-streamlit.title('my parents new healthy dinner')
-streamlit.header('Breakfast Favotite')
-streamlit.text('🥣 omega-3 blueberry oatmeal')
-streamlit.text('🥗 kale,spinach&rocket smoothie')
-streamlit.text('🐔 hard boiled free-ramged egg')
-streamlit.text('🥑🍞 Avacado toast')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 
