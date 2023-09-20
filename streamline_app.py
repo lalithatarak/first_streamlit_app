18423a9562b4e5b88f8473c5afb7a03f9f264fb6
lalithatarak <145041515+lalithatarak@users.noreply.github.com>
2023-09-20 13:00:29 -0400

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -16,6 +16,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 streamlit.header("Fruityvice Fruit Advice!")
+streamlit.dataframe(fruityvice_normalized)
 
