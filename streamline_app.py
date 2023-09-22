dcfbb8d7830ff6591889d294cdf48efcc977cdf9
lalithatarak <145041515+lalithatarak@users.noreply.github.com>
2023-09-22 13:26:39 -0400

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -1,11 +1,12 @@
 import snowflake
 import snowflake.connector
+from urllib.error import URLERROR
 import streamlit
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
-import streamlit
-import pandas
+
+
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
