25bda78801a24bd3b5bb02e97db5798664023a80
lalithatarak <145041515+lalithatarak@users.noreply.github.com>
2023-09-22 14:06:43 -0400

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -62,7 +62,18 @@ try:
     streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
     streamlit.error()
-
+def get_fruityvice_data(this_fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ this_fruit_choice)
+    fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+streamlit.header("Fruityvice Fruit Advice!")
+try:
+   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+   if not fruit_choice:
+    streamlit.error("please select a fruit to get information")
+   else:
+    back_from_function = get fruity_vice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
     
 
 
