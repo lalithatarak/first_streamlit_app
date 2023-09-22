db3abb691071c51412497306dfa4d14d433169cb
lalithatarak <145041515+lalithatarak@users.noreply.github.com>
2023-09-22 14:08:33 -0400

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -72,7 +72,7 @@ try:
    if not fruit_choice:
     streamlit.error("please select a fruit to get information")
    else:
-    back_from_function = get fruity_vice_data(fruit_choice)
+    back_from_function = get fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
     
 
