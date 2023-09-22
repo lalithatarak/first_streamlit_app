f9a4c5cc7ae16e6d4012b5abc3158cbad65d0f77
lalithatarak <145041515+lalithatarak@users.noreply.github.com>
2023-09-22 13:45:04 -0400

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -51,7 +51,21 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add','Jackfruit')
 streamlit.write('Thanks for adding  ', add_my_fruit)
 my_cur.execute("insert into fruit_load_list values('from streamlit')") 
+streamlit.header("Fruityvice Fruit Advice!")
+try:
+  
 
+    fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+    streamlit.error("please select a fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "kiwi")
+    fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+streamlit.error()
+
+    
 
 
 
