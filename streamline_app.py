ec0643cc609fb3ae4f4b1f889ac3f7df9c0f4305
lalithatarak <145041515+lalithatarak@users.noreply.github.com>
2023-09-22 22:29:10 -0400

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -95,5 +95,10 @@ if streamlit.button('Add a Fruit to the List'):
     back_from_function = insert_row_snowflake(add_my_fruit)
     my_cnx.close()
 
+if streamlit.button('get fruit list'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    my_cnx.close()
+    streamlit.dataframe(my_data_rows)
 
 
