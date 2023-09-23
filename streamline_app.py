57e03770d965bcb5f9250a1dccfa0422dba93644
lalithatarak <145041515+lalithatarak@users.noreply.github.com>
2023-09-22 21:48:20 -0400

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -52,30 +52,21 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add','Jackfrui
 streamlit.write('Thanks for adding  ', add_my_fruit)
 my_cur.execute("insert into fruit_load_list values('from streamlit')") 
 streamlit.header("Fruityvice Fruit Advice!")
-try:
-   fruit_choice = streamlit.text_input('What fruit would you like information about?')
-   if not fruit_choice:
-    streamlit.error("please select a fruit to get information")
-   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "kiwi")
-    fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
-except URLError as e:
-    streamlit.error()
+
 def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ this_fruit_choice)
     fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
 streamlit.header("Fruityvice Fruit Advice!")
-#try:
-   #fruit_choice = streamlit.text_input('What fruit would you like information about?')
-   #if not fruit_choice:
-        #streamlit.error("please select a fruit to get information")
-   #else:
-        #back_from_function = get_fruityvice_data(fruit_choice)
-        #streamlit.dataframe(back_from_function)
-#except URLError as e:
-    #streamlit.error()#
+try:
+   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+   if not fruit_choice:
+        streamlit.error("please select a fruit to get information")
+   else:
+        back_from_function = get_fruityvice_data(fruit_choice)
+        streamlit.dataframe(back_from_function)
+except URLError as e:
+    streamlit.error()
     
 
 
